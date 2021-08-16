0ebabe1435af43f58513c94a192fc8c974cdb1ce
Jair Alayon <jair.atl.95@gmail.com>
2021-08-15 21:59:46 -0500

    Labels examples

---
+++ labels/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	outer := 19
+	_ = outer
+	people := [5]string{"Jair", "Antonio", "Mariana", "Michael", "Brenda"}
+	friends := [2]string{"Michael", "Luis"}
+
+outer:
+	for index, name := range people {
+		for _, friend := range friends {
+			if name == friend {
+				fmt.Printf("Found a friend %q at index %d\n", friend, index)
+				break outer
+			}
+		}
+	}
+
+	fmt.Println("Next instruction after the break")
+}
